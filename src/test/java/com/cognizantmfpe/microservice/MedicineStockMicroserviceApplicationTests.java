f45c66581392d71f412f2beb2ae376cac4843a57
harrykan07 <rianhsirah04@gmail.com>
2021-07-29 17:05:20 +0530

    Changes

---
+++ src/test/java/com/cognizantmfpe/microservice/MedicineStockMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizantmfpe.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineStockMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
